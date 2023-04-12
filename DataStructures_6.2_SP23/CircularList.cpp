95c672096d2877be267a33307d1383688260cc63
arcadiobastidas <abastidas31@gmail.com>
2023-04-12 08:23:28 -0400

    circular list missing testing and overloaded functions

---
+++ DataStructures_6.2_SP23/CircularList.cpp
@@ -0,0 +1,64 @@
+﻿#include "CircularList.h"
+template<class ItemType>
+CircularList<ItemType>::CircularList() : listPtr(new LinkedList<ItemType>())
+{
+}
+
+template<class ItemType>
+CircularList<ItemType>::~CircularList()
+{
+    clear();
+}
+
+template<class ItemType>
+bool CircularList<ItemType>::isEmpty() const
+{
+    return listPtr->isEmpty();
+}
+
+template<class ItemType>
+int CircularList<ItemType>::getLength() const
+{
+    return listPtr->getLength();
+}
+
+template<class ItemType>
+bool CircularList<ItemType>::insert(int newPosition, const ItemType& newEntry)
+{
+    bool isInserted = false;
+
+    if (newPosition >= 1 && newPosition <= getLength() + 1)
+    {
+        isInserted = listPtr->insert(newPosition, newEntry);
+    }
+
+    return isInserted;
+}
+
+template<class ItemType>
+bool CircularList<ItemType>::remove(int position)
+{
+    bool isRemoved = false;
+
+    if (position >= 1 && position <= getLength())
+    {
+        isRemoved = listPtr->remove(position);
+    }
+
+    return isRemoved;
+}
+
+template<class ItemType>
+void CircularList<ItemType>::clear()
+{
+    while (!isEmpty())
+    {
+        remove(1);
+    }
+}
+
+template<class ItemType>
+ItemType CircularList<ItemType>::getEntry(int position) const throw(PrecondViolatedExcep)
+{
+    return listPtr->getEntry(position);
+}
---
+++ DataStructures_6.2_SP23/CircularList.h
@@ -0,0 +1,23 @@
+﻿#ifndef CIRCULAR_LIST_H
+#define CIRCULAR_LIST_H
+
+#include "LinkedList.h"
+
+template<class ItemType>
+class CircularList
+{
+private:
+    LinkedList<ItemType>* listPtr;  // Pointer to the circular linked list
+public:
+    CircularList();
+    ~CircularList();
+    bool isEmpty() const;
+    int getLength() const;
+    bool insert(int newPosition, const ItemType& newEntry);
+    bool remove(int position);
+    void clear();
+    ItemType getEntry(int position) const throw(PrecondViolatedExcep);
+
+
+};
+#endif
